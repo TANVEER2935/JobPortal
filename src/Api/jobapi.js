d1f6e5e9b9101a9c7f02699ec1f7c324e9c16bda
TANVEER2935 <160842616+TANVEER2935@users.noreply.github.com>
2025-07-02 15:27:03 +0530

    Update jobapi.js

--- src/Api/jobapi.js
+++ src/Api/jobapi.js
@@ -4,6 +4,11 @@ const API = axios.create({
   baseURL: "https://686389e088359a373e953dc8.mockapi.io/", 
 });
 
-export const getjob = () => API.get("/job");
+export const getjob = async () => {
+  const response = await fetch("https://686389e088359a373e953dc8.mockapi.io/job");
+  const data = await response.json();
+  return data; // this must be an array!
+};
+
 export const createjob = (data) => API.post("/job", data);
 export const deletejob = (id) => API.delete(`/job/${id}`);
